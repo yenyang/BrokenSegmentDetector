2ddd91ab9f32b28d53a34ed43acf722e8f307767
yenyang <tjkelley19@gmail.com>
2024-10-29 14:30:38 -0700

    added preview image added thumbail blocked out but preserved code for removing nodes without edges added empty changelog add License.Txt file added long description added read me.

--- BrokenSegmentDetector/Mod.cs
+++ BrokenSegmentDetector/Mod.cs
@@ -16,16 +16,20 @@ namespace BrokenSegmentDetector
     {
         public static ILog log = LogManager.GetLogger($"{nameof(BrokenSegmentDetector)}").SetShowsErrorsInUI(false);
 
+        /// <inheritdoc/>
         public void OnLoad(UpdateSystem updateSystem)
         {
             log.Info(nameof(OnLoad));
 
             if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
+            {
                 log.Info($"Current mod asset at {asset.path}");
+            }
 
             updateSystem.UpdateAt<FindBrokenNodesSystem>(SystemUpdatePhase.GameSimulation);
         }
 
+        /// <inheritdoc/>
         public void OnDispose()
         {
             log.Info(nameof(OnDispose));
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # BrokenSegmentDetector
+
+See Long Description: https://github.com/yenyang/BrokenSegmentDetector/blob/master/BrokenSegmentDetector/LongDescription.md
